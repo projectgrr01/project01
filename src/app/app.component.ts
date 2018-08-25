0fbb440c2e52e9c1d1d1525605ea33f7541e16a3
projectgrr01 <projectgrr01@grr.la>
2018-08-25 13:27:03 +0530

    updated code to bring loader on the first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../assets/css/styles.css']
 })
 export class AppComponent {
   title = 'my-project';
