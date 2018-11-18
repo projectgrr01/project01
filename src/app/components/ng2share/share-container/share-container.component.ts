1e3ea7a9371ed3ec3433d65ed338ba2c11aa7499
projectgrr01 <projectgrr01@grr.la>
2018-11-18 20:01:01 +0530

    added whatsapp and download icons and functinality, download is pending due to gif url not having extension in it

--- src/app/components/ng2share/share-container/share-container.component.ts
+++ src/app/components/ng2share/share-container/share-container.component.ts
@@ -2,8 +2,9 @@ import { Inject } from '@angular/core';
 import { WINDOW } from '@ng-toolkit/universal';
 import { Component, Input, OnInit, ViewEncapsulation}
         from '@angular/core';
-import { trigger,state,style,transition,animate } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Properties } from '../properties.utils'
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'share-container',
@@ -12,39 +13,41 @@ import { Properties } from '../properties.utils'
   animations: [
           trigger('expandedState', [
             state('expanded', style({
-              width:'*',
-              height:'*'
+              width: '*',
+              height: '*'
             })),
             state('collapsed', style({
-              width:'0',
-              height:'0'
+              width: '0',
+              height: '0'
             })),
             transition('collapsed <=> expanded', animate('100ms ease-in'))
           ])
         ]
 })
-export class ShareContainerComponent implements OnInit{
+export class ShareContainerComponent implements OnInit {
   // Primary platforms that appear
   @Input() platforms = ['twitter', 'facebook'];
   // Secondary Platforms that appear when expanded
-  @Input() secondaryPlatforms =  ['googlePlus','reddit','pinterest', 'linkedin'];
+  @Input() secondaryPlatforms =  ['googlePlus', 'reddit', 'pinterest', 'linkedin'];
   // Wether or not the component is expendable
   @Input() expandable:boolean = true;
   // tells if the text must be enabled on primary platforms
   @Input() textEnabled:boolean = false;
   // Text added to the vanilla message, ex: 'your creation' will result in
   // 'Tweet your creation' for twitter or 'Share your creation' for fb
-  @Input() addedText:string;
+  @Input() addedText: string;
   // This should be set up directly in the meta tags as this is good practice
   // Use this input only if you have multiple content to share per url.
   // So in case you need this the input should be like the following object (you can omitt some fields)
   // {title:'my title', description:'my desc',img:' an image', via:'Ced_VDB', hashtags:'someHashTag'}
-  @Input() properties:Properties = {};
+  @Input() properties: Properties = {};
+  @Input() shortenUrl: String = '';
   // horizontal layout or vertical layout (_accessed via getter & setter)
-  _direction:string = 'horizontal';
+  _direction: String = 'horizontal';
   // state of the secondary platform expandable pannel
-  expandedState:string = "collapsed";
- constructor(@Inject(WINDOW) private window: Window) {}
+  expandedState: String = 'collapsed';
+
+  constructor(@Inject(WINDOW) private window: Window, private sanitization: DomSanitizer) {}
 
 
   ngOnInit(){
@@ -83,5 +86,13 @@ export class ShareContainerComponent implements OnInit{
 
   get direction(){
     return this._direction;
+  }
+
+  public getWhatsappShareableUrl() {
+    return this.sanitization.bypassSecurityTrustResourceUrl('https://api.whatsapp.com/send?text=' + this.shortenUrl);
+  }
+
+  public getDownloadableUrl() {
+    return this.sanitization.bypassSecurityTrustResourceUrl(this.shortenUrl.toString());
   }
 }
